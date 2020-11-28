f32836ba2b370b9da4b2f3db5b72d6742a98ee78
PC <PC@host.docker.internal>
2020-11-27 23:37:48 -0500

    version12

---
+++ src/main/java/com/mycompany/mavenproject2/Employe.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.mavenproject2;
+
+/**
+ *
+ * @author PC
+ */
+public class Employe {
+    
+    public static void afficher (){
+    
+        
+        System.out.println("hhhhhhhhhhhhhhh");
+    }
+    
+}
